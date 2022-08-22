1832f062933ae36762d23db164bf2346c8d93cfc
Hyo-jung Lee <leeexpert@cau.ac.kr>
2022-08-22 16:11:06 +0900

    220822

--- BOJ/220822/11729.py
+++ BOJ/220822/11729.py
@@ -2,13 +2,13 @@ import sys
 
 n=int(sys.stdin.readline().strip())
 tower=[i for i in range(1, n+1)]
-count=0
 def hanoi(a, b, n):
     if n==1:
         print(a, b)
         return
-    hanoi(a, 6-a-b, n-1)
-    print(a, b)
-    hanoi(6-a-b, b, n-1)
-
+    else:
+        hanoi(a, 6-a-b, n-1)
+        print(a, b)
+        hanoi(6-a-b, b, n-1)
+print(2**n-1)
 hanoi(1, 3, n)
