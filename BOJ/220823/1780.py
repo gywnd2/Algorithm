6ea02be5096135d587c7f2e880a4468e1f10ffa1
Hyo-jung Lee <leeexpert@cau.ac.kr>
2022-08-24 11:27:04 +0900

    220824

--- BOJ/220823/1780.py
+++ BOJ/220823/1780.py
@@ -3,27 +3,25 @@ import sys
 n=int(sys.stdin.readline().strip())
 paper=[list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
 
-minus=0
-zero=0
-one=0
-
-def total(r, c):
-    tmp=sum(paper[r][c:c+3])
-    tmp+=sum(paper[r+1][c:c+3])
-    tmp+=sum(paper[r+2][c:c+3])
-    return tmp
-
-for i in range(0, n, 3):
-    for j in range(0, n, 3):
-        totalSum=total(i, j)
-        if totalSum==0:
-            print("zero!")
-            zero+=1
-        elif totalSum==9:
-            print("one!")
-            one+=1
-        elif totalSum==-9:
-            print("minus!")
-            minus+=1
-        else:
-            print('error!')
+minus=0; zero=0; one=0
+def check(x, y, n):
+    global minus, zero, one
+    num=paper[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if paper[i][j]!=num:
+                for k in range(3):
+                    for l in range(3):
+                        check(x+k*n//3, y+l*n//3, n//3)
+                return
+    if num==1:
+        one+=1
+    elif num==-1:
+        minus+=1
+    elif num==0:
+        zero+=1
+        
+check(0, 0, n)    
+print(minus)
+print(zero)
+print(one)
