add6a0d90b9ff94d7439a1e1b9c92d2ea4375742
Hyo-jung Lee <leeexpert@cau.ac.kr>
2022-08-26 00:03:18 +0900

    220826

--- BOJ/220825/2447.py
+++ BOJ/220825/2447.py
@@ -1,16 +1,14 @@
 import sys
 
 n=int(sys.stdin.readline().strip())
-
+count=0
 def box(x, y, n):
-    if n==3:
-        print("***")
-        print("* *")
-        print("***")
-        
-print("***")
-print("* *")
-print("***", end='')
-print("***")
-print("* *")
-print("***", end='')
+    global count
+    print(count)
+    if n//3!=0:
+        for i in range(x, x+n, n//3):
+            for j in range(y, y+n, n//3):
+                box(0, 0, n//3)
+                count+=1
+        return
+box(0, 0, n)
