e643c73b9a9e9182ae405609f9ab0412f54b9d60
Hyojung Lee <leeexpert@cau.ac.kr>
2023-01-02 16:42:58 +0900

    230102

--- YeeCOTE/221020/미래 도시.py
+++ YeeCOTE/221020/미래 도시.py
@@ -1,26 +1,27 @@
-import sys, heapq
-n, m=map(int, sys.stdin.readline().strip().split())
-graph=[[sys.maxsize for _ in range(n+1)] for _ in range(n+1)]
+import heapq
 
-for a in range(1, n+1):
-    for b in range(1, n+1):
+N, M=map(int, input().split())
+
+graph=[[int(1e9) for _ in range(N+1)] for _ in range(N+1)]
+
+for _ in range(M):
+    a, b=map(int, input().split())
+    graph[a][b]=1
+    graph[b][a]=1
+
+for a in range(N+1):
+    for b in range(N+1):
         if a==b:
             graph[a][b]=0
+    
+X, K=map(int,input().split())
 
-for _ in range(m):
-    start, dest=map(int, sys.stdin.readline().strip().split())
-    graph[start][dest]=1
-    graph[dest][start]=1
-
-x, k=map(int, sys.stdin.readline().strip().split())
-
-for k in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
+for k in range(1, N+1):
+    for a in range(1, N+1):
+        for b in range(1, N+1):
             graph[a][b]=min(graph[a][b], graph[a][k]+graph[k][b])
 
-res=graph[1][k]+graph[k][x]
-if res>=sys.maxsize:
+if graph[1][K]+graph[K][X]>=int(1e9):
     print(-1)
 else:
-    print(res)
+    print(graph[1][K]+graph[K][X])
